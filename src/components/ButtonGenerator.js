abe0c77a0903de1b59db9de2db410cfc483cf348
Andrew Sidorchuk <sidorchukandrew@gmail.com>
2021-01-14 19:00:04 -0500

    Changed full width to flex grow

--- src/components/ButtonGenerator.js
+++ src/components/ButtonGenerator.js
@@ -1,25 +1,34 @@
+import Checkbox from "@material-ui/core/Checkbox";
+
 function ButtonGenerator() {
     return (
         <div className="p-vertical-lg">
 
-            <div className="p-vertical-md">
+            <div className="p-vertical-md d-flex f-column">
                 <label className="font-xs bold-5">
                     REDIRECT URI
                 </label>
-                <input className="foc-ring rounded-sm p-md main-font grey-border full-width"></input>
+                <input className="foc-ring rounded-sm p-md main-font grey-border flex-grow"></input>
             </div>
-            <div className="p-vertical-md ">
+            <div className="p-vertical-md d-flex f-column">
                 <label className="font-xs bold-5">
                     CLIENT ID
                 </label>
-                <input className="foc-ring rounded-sm p-md main-font grey-border full-width"></input>
+                <input className="foc-ring rounded-sm p-md main-font grey-border flex-grow"></input>
             </div>
 
-            <div className="p-vertical-md ">
+            <div className="p-vertical-md d-flex f-column">
                 <label className="font-xs bold-5">
                     SCOPES
                 </label>
-               <div className="rounded-sm p-md grey-bg-4 grey-border">Scopes in here</div>
+                <div className="rounded-sm p-md grey-bg-4 grey-border">
+                    <div className="d-flex align-center">
+                        <Checkbox color="#50abf0" />
+                        <span className="font-sm">
+                            identity
+                        </span>
+                    </div>
+                </div>
             </div>
         </div>
     )
