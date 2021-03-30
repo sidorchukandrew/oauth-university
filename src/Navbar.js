4acd79984c5bfc54d7ff4c37b2d326ab1a7e6dad
Andrew Sidorchuk <sidorchukandrew@gmail.com>
2021-03-30 18:56:30 -0400

    Added new logo

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,6 +3,7 @@ import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { Fragment } from "react";
 import { useTheme } from '@material-ui/core/styles';
 import MenuIcon from "@material-ui/icons/MenuRounded";
+import Logo from "./logo.png";
 
 export default function Navbar(props) {
 
@@ -20,34 +21,45 @@ export default function Navbar(props) {
 
     let navLinks = (
         <Fragment>
-            <NavLink
-                activeClassName="active-link"
-                to="/"
-                className="rounded-sm grey-text-2 p-vertical-sm p-horiz-md m-horiz-sm hov-lighten"
-                exact
-            >
-                Home
-            </NavLink>
-            <NavLink
-                activeClassName="active-link"
-                to="/guides"
-                className="rounded-sm grey-text-2 p-vertical-sm p-horiz-md m-horiz-sm hov-lighten"
-            >
-                Guides
-            </NavLink>
-            <NavLink
-                activeClassName="active-link"
-                to="/series"
-                className="rounded-sm grey-text-2 p-vertical-sm p-horiz-md m-horiz-sm hov-lighten"
-            >
-                Series
+            <div className="d-flex align-center white-text">
+                <NavLink to="/">
+                    <img src={Logo} alt="Logo" style={{ height: "35px" }} />
                 </NavLink>
-        </Fragment>
+                <span className="p-horiz-md bold-5">
+                    OAuth University
+                </span>
+            </div>
+            <div>
+
+                <NavLink
+                    activeClassName="active-link"
+                    to="/"
+                    className="rounded-sm grey-text-2 p-vertical-sm p-horiz-md m-horiz-sm hov-lighten"
+                    exact
+                >
+                    Home
+            </NavLink>
+                <NavLink
+                    activeClassName="active-link"
+                    to="/guides"
+                    className="rounded-sm grey-text-2 p-vertical-sm p-horiz-md m-horiz-sm hov-lighten"
+                >
+                    Guides
+            </NavLink>
+                <NavLink
+                    activeClassName="active-link"
+                    to="/series"
+                    className="rounded-sm grey-text-2 p-vertical-sm p-horiz-md m-horiz-sm hov-lighten"
+                >
+                    Series
+                </NavLink>
+            </div>
+        </Fragment >
     );
 
     let classes = "primary-color-bg d-flex align-center p-vertical-md"
     if (!isSmallScreen) {
-        classes += " p-horiz-xl justify-end";
+        classes += " p-horiz-xl justify-space-between";
     }
 
     return (
